eec38e19ff946ecae42c7d388a62f3d5b96918f5
Tiagko Amersonis <thiago.amerssonis@gmail.com>
2021-07-25 00:42:21 +0100

    Added Angular Framework + template header and assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mochkai-github-io';
+  title = 'Mochkai\'s Portfolio';
+  
 }

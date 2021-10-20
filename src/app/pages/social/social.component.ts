da234631ab4c8e9a1b30341184716640f852a5dd
MochKai <thiago.amerssonis@gmail.com>
2021-10-20 08:34:10 +0100

    Added live stream

--- src/app/pages/social/social.component.ts
+++ src/app/pages/social/social.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ScriptService } from 'src/app/services/script.service';
+
+declare let Twitch: any;
 
 @Component({
   selector: 'app-social',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SocialComponent implements OnInit {
 
-  constructor() { }
+  private scriptLoader: Promise<any>;
+
+  constructor(private scriptService: ScriptService) {
+    this.scriptLoader = this.scriptService.load('twitchEmbed');
+  }
 
   ngOnInit(): void {
+    this.scriptLoader.then((script) => {
+      if (script[0].loaded) {
+        new Twitch.Embed("twitch-live", {
+          width: '100%',
+          height: 600,
+          theme: 'dark',
+          channel: "mochkai",
+          autoplay: true
+        });
+      }
+    });
   }
 
 }
